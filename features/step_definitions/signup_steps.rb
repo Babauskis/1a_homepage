1b73a68eb02cb7fd4612ecd79f93b38bbf61675b
Sandis Babauskis <v-sababa@microsoft.com>
2018-12-27 16:21:46 +0200

    Added functionality to validate currency for first 4 items

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -11,11 +11,22 @@ And(/^I verify if first item is visible/) do
   unless find(:css, "img[alt='JBL Tune 500BT Bluetooth On-Ear Headphones Black']").visible?
     raise "first item is not visible"
   end
+if find(:xpath, '//a[contains(., "JBL Tune 500BT Bluetooth On-Ear Headphones Black")]/ancestor::div[@class="p-content"]/following-sibling::div[@class="info-holder"]/child::div[@class="p-info"]//child::div[@data-sell-price-w-vat="41.99"]').visible?
+    puts 'Test passed'
+  else
+    puts 'Price for first item was not equal'
+  end
 end
 
 And(/^I verify if second item is visible/) do
 unless find(:css, "img[alt='JBL T450BT Bluetooth On-Ear Headphones White']").visible?
   raise "second item is not visible"
+  end
+
+  if find(:xpath, '//a[contains(., "JBL T450BT Bluetooth On-Ear Headphones White")]/ancestor::div[@class="p-content"]/following-sibling::div[@class="info-holder"]/child::div[@class="p-info"]//child::div[@data-sell-price-w-vat="66.56"]').visible?
+    puts 'Test passed'
+  else
+    puts 'Price for second item was not equal'
   end
 end
 
@@ -23,11 +34,21 @@ And(/^I verify if third item is visible/) do
   unless find(:css, "img[alt='JBL GO 2 Bluetooth Speaker Midnight Black']").visible?
     raise "third item is not visible"
   end
+  if find(:xpath, '//a[contains(., "JBL GO 2 Bluetooth Speaker Midnight Black")]/ancestor::div[@class="p-content"]/following-sibling::div[@class="info-holder"]/child::div[@class="p-info"]//child::div[@data-sell-price-w-vat="29.99"]').visible?
+    puts 'Test passed'
+  else
+    puts 'Price for third item was not equal'
+  end
 end
 
 
 And(/^I verify if fourth item is visible/) do
   unless find(:css, "img[alt='JBL GO 2 Bluetooth Speaker Midnight Black']").visible?
     raise "fourth item is not visible"
+  end
+  if find(:xpath, '//a[contains(., "JBL GO 2 Bluetooth Speaker Midnight Black")]/ancestor::div[@class="p-content"]/following-sibling::div[@class="info-holder"]/child::div[@class="p-info"]//child::div[@data-sell-price-w-vat="31.00"]').visible?
+    puts 'Test passed'
+  else
+    puts 'Price for fourth item was not equal'
   end
 end
